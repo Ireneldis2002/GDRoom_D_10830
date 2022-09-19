d65634eadcdc4c22c743a6f0f20fc616b779b962
Ireneldis2002 <reneija6@gmail.com>
2022-09-19 20:59:37 +0700

    Upload Guided Room

---
+++ app/src/main/java/com/romm/gdroom_d_10830/room/Note.kt
@@ -0,0 +1,12 @@
+package com.romm.gdroom_d_10830.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
